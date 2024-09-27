9938114de47db2557d7f76970c5da92e48e3adfb
Margarita Potylitsyna <148562840+Ritumka@users.noreply.github.com>
2024-09-27 23:15:19 +0000

    done!

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -5,14 +5,28 @@ class DirectorsController < ApplicationController
 
 
   def youngest
+
+    @youngest_dob  = Director.where({ :name => ("A".."Z") }).maximum(:dob)
+    
+    youngest = Director.where({ :dob => @youngest_dob })
+    @youngest_id = youngest.at(0).id
+    @youngest_name = youngest.at(0).name
+
     render({ :template => "director_templates/youngest" })
   end
 
   def eldest
+
+    @eldest_dob  = Director.where({ :name => ("A".."Z") }).minimum(:dob)
+    
+    eldest = Director.where({ :dob => @eldest_dob })
+    @eldest_id = eldest.at(0).id
+    @eldest_name = eldest.at(0).name
+    
     render({ :template => "director_templates/eldest" })
   end
 
-  
+
   def show
     the_id = params.fetch("the_id")
 
